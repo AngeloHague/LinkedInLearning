8d3b2c5202b17fe746f75d7ea7de41f548e94061
Angelo Hague <angelohague@outlook.com>
2025-04-04 20:35:50 +0100

    Comments

---
+++ 3. C#/1. Overview/1.4 Comments/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Looks like you need to use this structure for XML comments, didn't work with the new console template
+namespace MyApp
+{
+    internal class Program
+    {
+        // Single-line comment
+
+        /* Multi-line comment
+           Another line of comment
+        */
+
+        /// XML Comments
+        /// <summary>
+        /// This is a summary of the class
+        /// </summary>
+        /// <param name="args"> An array of string arguments from CLI</param>
+        /// <returns> Returns nothing</returns>
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
