a153e970d60254a9d89814882c38707193a39082
Angelo Hague <angelohague@outlook.com>
2025-04-05 17:59:33 +0100

    Palindrome code challenge solution

---
+++ 3. C#/4. Functions/5. Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿// C# code​​​​​​‌‌​‌​​​‌​‌​‌‌‌​​​​​​‌​​​‌ below
+using System;
+using System.Text;
+
+// Determine whether a string is a Palindrome
+static bool IsPalindrome(string thestr) {
+    thestr = thestr.ToLower();
+    foreach (char c in thestr){
+        if (char.IsPunctuation(c) || char.IsWhiteSpace(c)) {
+            thestr = thestr.Replace(c.ToString(), "");
+        }
+    };
+    char[] charArray;
+    charArray = thestr.ToCharArray();
+    Array.Reverse(charArray);
+    Console.WriteLine($"'{thestr}' vs {charArray.ToString()}");
+    for (int i = 0; i < charArray.Length; i++) {
+        if (thestr[i] != charArray[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+string[] teststrings = { "Hello World!", "Race car!", "Rotor", "More cowbell!", "Madam, I'm Adam." };
+int palcount = 0;
+foreach (string str in teststrings) {
+	bool learnerResult = IsPalindrome(str);
+	if (learnerResult)
+		palcount++;
+}Console.WriteLine($"There are {palcount} palindromes in the test strings.");
