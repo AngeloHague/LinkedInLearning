77a4ae54ed71d74be6cf26f8adda92bfb5b5ad4d
Angelo Hague <angelohague@outlook.com>
2025-04-05 12:09:56 +0100

    Using String Builder

---
+++ 3. C#/3. Strings/4. String builder/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jumpCount = 10;
+            string[] animals = {"goats", "cats", "pigs"};
+
+            // TODO: create a StringBuilder
+            StringBuilder sb = new StringBuilder("Initial string.", 200); // initial value, max length
+
+            // TODO: print some basic stats about the StringBuilder
+            Console.WriteLine($"\nCapacity: {sb.Capacity}; Length: {sb.Length}");
+
+            // TODO: Add some strings to the builder using Append
+            sb.Append(" Hello,");
+            sb.Append(" Jim!");
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine($"\nCapacity: {sb.Capacity}; Length: {sb.Length}");
+
+            // TODO: AppendLine can append a line ending
+            sb.AppendLine();
+            sb.AppendLine("This is story about Jim.");
+            // Console.WriteLine(sb.ToString());
+
+            // TODO: AppendFormat can be used to append formatted strings
+            sb.AppendFormat("He jumped {0} times.", jumpCount);
+
+            // TODO: AppendJoin can iterate over a set of values
+            sb.AppendLine(" He jumped over; ");
+            sb.AppendJoin(", ", animals);
+            // Console.WriteLine(sb.ToString());
+
+            // TODO: Modify the content using Replace
+            sb.Replace("cats", "dogs");
+            // Console.WriteLine(sb.ToString());
+
+            // TODO: Insert content at any index
+            sb.Insert(0, "This is the ");
+            
+            // TODO: Convert to a single string
+            Console.WriteLine($"\nCapacity: {sb.Capacity}; Length: {sb.Length}");
+            Console.WriteLine(sb.ToString());
+
+        }
+    }
+}
