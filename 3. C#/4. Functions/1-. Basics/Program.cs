04b6acc46ea564d6129ba04b0c34f6bd5cddafdc
Angelo Hague <angelohague@outlook.com>
2025-04-05 17:26:11 +0100

    Basics and params (named and default)

---
+++ 3. C#/4. Functions/1-. Basics/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Typed function
+float MilesToKm(float miles) {
+    float res = miles * 1.6f;
+    return res;
+}
+
+float miles = 10.0f;
+Console.WriteLine($"{miles} Miles in km: {MilesToKm(miles)}");
+
+// Void function with named and default params
+void PrintWithPrefix(string str, string prefix="") {
+    Console.WriteLine($"{prefix} {str}");
+}
+
+PrintWithPrefix("hello!",":: >");
+PrintWithPrefix("hello!"); // no prefix, use default
+
+// call with named params:
+PrintWithPrefix(prefix: "!!", str: "hello!"); // no prefix, use default
+
