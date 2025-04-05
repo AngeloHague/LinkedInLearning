e2139e1ea60b878ce747030af3089751d3221745
Angelo Hague <angelohague@outlook.com>
2025-04-05 12:00:15 +0100

    String interpolation

---
+++ 3. C#/3. Strings/3. String interpolation/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+// Declare some variables
+string make = "Mercedes-Benz";
+string model = "G Class";
+int year = 2020;
+float miles = 8_450.27f;
+decimal price = 60_275.0m;
+
+// TODO: Output information using formatting
+Console.WriteLine("This car is a {0} {1} {2} with {3} miles and costs ${4:C}", year, make, model, miles, price);
+
+// TODO: Using string interpolation
+Console.WriteLine("\nLooks like we already did this in earlier exercises;)");
+Console.WriteLine($"This car is a {year} {make} {model} with {miles} miles and costs ${price:C}");
+
+// TODO: With inline expressions
+Console.WriteLine($"This car is a {year} {make} {model} with {{{miles * 1.6:F2}}} miles and costs ${price:C}");
