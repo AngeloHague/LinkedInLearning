8dd5d6eea32fb8ba1471c7f433dbd668e171e26c
Angelo Hague <angelohague@outlook.com>
2025-04-06 18:01:26 +0100

    Calling gRPC Services with a client

---
+++ 4. Learning ASP.NET Core/5. gRPC APIs/grpc-client/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Grpc.Net.Client;
+using grpc_client;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5145");
+
+var webDevClient = new WebDev.WebDevClient(channel);
+
+var webDevReply = webDevClient.CreateProject( new Project { Name = "MyProject", Id = 1 });
+
+Console.WriteLine($"{webDevReply}");
