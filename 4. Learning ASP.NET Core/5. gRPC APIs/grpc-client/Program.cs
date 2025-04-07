4b965ebd5bed8a3b6d8418dae2be14cee95ec695
Angelo Hague <angelohague@outlook.com>
2025-04-07 20:06:22 +0100

    Testing code challenge with the previous client

--- 4. Learning ASP.NET Core/5. gRPC APIs/grpc-client/Program.cs
+++ 4. Learning ASP.NET Core/5. gRPC APIs/grpc-client/Program.cs
@@ -1,11 +1,22 @@
-﻿
+﻿// Console.WriteLine("Hello, World!");
 using Grpc.Net.Client;
 using grpc_client;
 
-var channel = GrpcChannel.ForAddress("http://localhost:5145");
+// var channel = GrpcChannel.ForAddress("http://localhost:5145");
 
-var webDevClient = new WebDev.WebDevClient(channel);
+// var webDevClient = new WebDev.WebDevClient(channel);
 
-var webDevReply = webDevClient.CreateProject( new Project { Name = "MyProject", Id = 1 });
+// var webDevReply = webDevClient.CreateProject( new Project { Name = "MyProject", Id = 1 });
 
-Console.WriteLine($"{webDevReply}");
+// Console.WriteLine($"{webDevReply}");
+
+// Code Challenge
+var channel = GrpcChannel.ForAddress("http://localhost:5201");
+
+var booksClient = new Books.BooksClient(channel);
+
+var reply = booksClient.GetAuthor( new GetBookAuthorRequest { Title = "Test"});
+Console.WriteLine($"{reply}");
+
+reply = booksClient.GetAuthor( new GetBookAuthorRequest { Title = "1984"});
+Console.WriteLine($"{reply}");
